c0af039dfe753cad84307ef2adc0a7f81f3e3226
CarvalhoNathan <fariasnathan75@gmail.com>
2025-05-05 17:39:00 -0300

    Projeto finalizado

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+
+int numero;
+
+while (true)
+{
+    Console.Write("Digite um número: ");
+    bool conversaoValida = int.TryParse(Console.ReadLine(), out numero);
+
+    if (!conversaoValida || numero < 0)
+    {
+        Console.Clear();
+        Console.WriteLine("Digite um número inteiro válido!");
+    }
+    else
+    {
+        break;
+    }
+}
+
+Console.Clear();
+
+if (numero % 2 == 0)
+{
+    Console.WriteLine($"{numero} é par");
+}
+else
+{
+    Console.WriteLine($"{numero} é ímpar");
+}
